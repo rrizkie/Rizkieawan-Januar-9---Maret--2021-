50f9a04d88cba41bfe2e34291fbd9bfec6891dd3
Rizkiewan Januar <rizkieawan@Rizkiewans-MacBook-Pro.local>
2021-03-09 09:42:20 +0700

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+function palindrome (str){
+    let temp = ''
+    for (let i = str.length-1 ;i >= 0 ;i--){
+        temp+= str[i]
+    }
+    if (temp === str){
+        return 'ini palindrome'
+    }
+}
+
+console.log(palindrome('hannah'))
